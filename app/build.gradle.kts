8d53a08f46c8cdcf138d57b74ed0408802334171
Kousalya <kousalya.b@tringapps.com>
2024-02-07 16:08:25 +0530

    Scrollable appbar using material3

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -53,6 +53,7 @@ dependencies {
     implementation(libs.androidx.compose.material)
     implementation(libs.coil.compose)
     implementation(libs.livedata.compose)
+    implementation(libs.compose.material3)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
